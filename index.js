56bf3654f87b0f428b846ccb657a591212aed784
Gunasekhar-proclink <gunasekharreddy.chinamallappagari@proclink.com>
2024-06-05 15:03:52 +0530

    committed

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ console.log("Hello world ");
 console.log("feature1 commit");
 
 
-<<<<<<< Updated upstream
 
 console.log("Hello world ")
-=======
-console.log("stash commit")
->>>>>>> Stashed changes
 
 
 console.log("stash commit")
