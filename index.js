d4f72586cc7aafd2d1f0524ebc4fc59ff62e4721
Gunasekhar-proclink <gunasekharreddy.chinamallappagari@proclink.com>
2024-06-05 14:31:41 +0530

    commit after stash

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ console.log("Hello world ");
 console.log("feature1 commit");
 
 
+<<<<<<< Updated upstream
 
 console.log("Hello world ")
+=======
+console.log("stash commit")
+>>>>>>> Stashed changes
